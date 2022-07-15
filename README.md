f477b89cdb0a08affb504c0538546d4250b1f303
Nan Wang <nan.wang@jina.ai>
2022-07-15 12:12:23 +0800

    feat: first initialization

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# docsQA-status
-History of DocsQA service availability
+# DocsQA Status
+|          project          |Current Status|        Last Check        |      Last Downtime       |
+|---------------------------|--------------|--------------------------|--------------------------|
+|jina-ai/jina               |✅            |2022-07-15T11:59:07.192367|None                      |
+|jina-ai/finetuner          |✅            |2022-07-15T11:57:51.311675|None                      |
+|tiangolo/typer             |✅            |2022-07-15T11:59:38.097946|None                      |
+|tiangolo/sqlmodel          |✅            |2022-07-15T11:59:51.384212|None                      |
+|jina-ai/docarray           |✅            |2022-07-15T11:57:37.025665|None                      |
+|coqui-ai/TTS               |✅            |2022-07-15T11:58:44.437018|None                      |
+|coqui-ai/STT               |✅            |2022-07-15T12:00:21.966526|None                      |
+|kornia/kornia              |✅            |2022-07-15T11:58:33.616279|None                      |
+|jina-ai/clip-as-service    |⛔️           |2022-07-15T11:57:31.407902|2022-07-15T11:57:31.407882|
+|shazhou2015/clip-as-service|✅            |2022-07-15T11:58:24.498699|None                      |
+|garbles-dev/whitepaper     |✅            |2022-07-15T12:00:36.131699|None                      |
+|jina-ai/now                |✅            |2022-07-15T12:00:46.041223|None                      |
+|fogx/jina                  |✅            |2022-07-15T11:58:02.993433|None                      |
+|shan-mx/xiaohu-ai          |⛔️           |2022-07-15T11:59:51.096599|2022-07-15T11:59:51.096408|
+|alt-shreya/jina            |✅            |2022-07-15T12:00:03.942806|None                      |
+|wj-Mcat/python-wechaty     |✅            |2022-07-15T11:59:24.529843|None                      |
+|kivy/kivy                  |✅            |2022-07-15T11:58:44.172543|None                      |
---
+++ app.py
@@ -0,0 +1,147 @@
+import asyncio
+import json
+import os
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+
+import aiohttp
+from aiohttp.client_exceptions import ClientConnectorError
+from pytablewriter import MarkdownTableWriter
+
+
+class Status(str, Enum):
+    AVAILABLE = 'AVAILABLE'
+    UNAVAILABLE = 'UNAVAILABLE'
+
+
+@dataclass
+class HealthCheckEvent:
+    ctime: datetime
+    status: Status
+
+
+@dataclass
+class Project:
+    repo: str
+    name: str
+    host: str
+    status: Status = Status.AVAILABLE
+    last_utime: datetime = datetime.now()  # last health check time
+    last_dtime: Optional[datetime] = None  # last down time
+    history: Optional[List[HealthCheckEvent]] = None
+
+
+header = {
+    'Authorization': os.environ.get('DOCSQA_ADMIN_TOKEN'),
+    'Content-Type': 'application/json',
+}
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://apidocsqa.jina.ai/projects', headers=header) as resp:
+            json_body = await resp.json()
+            return json_body
+
+
+async def _health_check(project, session):
+    header = {'Content-Type': 'application/json', 'accept': 'application/json'}
+    payload = json.dumps({'data': [{'text': f'how to debug {project.repo}'}]})
+    url = os.path.join(project.host, 'search')
+    try:
+        async with session.post(url, data=payload, headers=header) as resp:
+            project.last_utime = datetime.now()
+            if resp.status != 200:
+                project.status = Status.UNAVAILABLE
+                project.last_dtime = datetime.now()
+                print(f'{project.host} response status error: {resp.status}')
+                return project
+            # check the response has matches
+            json_body = await resp.json()
+            data = json_body.get('data', None)
+            if data is None:
+                project.status = Status.UNAVAILABLE
+                project.last_dtime = datetime.now()
+                print(f'{project.host} empty response data: {json_body}')
+                return project
+            matches = data[0].get('matches', None)
+            if matches is None:
+                print(f'{project.host} empty matches: {json_body}')
+            project.status = Status.AVAILABLE if matches else Status.UNAVAILABLE
+            return project
+    except ClientConnectorError as e:
+        print(f'failed to connect to {project.host}, {e}')
+        project.status = Status.UNAVAILABLE
+        project.last_dtime = datetime.now()
+    finally:
+        project.last_utime = datetime.now()
+        return project
+
+
+async def health_check(projects_list):
+    tasks = []
+    custom_headers = {
+        'User-Agent': 'aiohttp client 0.17'
+    }
+
+    async with aiohttp.ClientSession(headers=custom_headers) as session:
+        # For each project put it into a task queue
+        for project in projects_list:
+            # send POST request to /search endpoint for each project
+            task = asyncio.ensure_future(_health_check(project, session))
+            tasks.append(task)
+        result = await asyncio.gather(*tasks)
+        return result
+
+
+def write_to_markdown(projects):
+    _matrix = []
+    for p in projects:
+        if p is None:
+            continue
+        _matrix.append([
+            p.repo,
+            '✅' if p.status == Status.AVAILABLE else '⛔️',
+            p.last_utime.isoformat(),
+            p.last_dtime.isoformat() if p.last_dtime is not None else 'None'
+        ])
+
+    # collect the data and write into a table
+    writer = MarkdownTableWriter(
+        table_name="DocsQA Status",
+        headers=['project', 'Current Status', 'Last Check', 'Last Downtime'],
+        value_matrix=_matrix,
+    )
+    # output the table into output.md
+    with open('README.md', 'w') as f:
+        output = writer.dumps()
+        f.write(output)
+
+
+def entrypoint():
+    # GET request to retrieve the project list
+    loop = asyncio.get_event_loop()
+    projects = loop.run_until_complete(main())
+    projects_list = []
+    for p in projects:
+        if p is None:
+            print(f'skip {project}')
+            continue
+        if p['status'] != 'PUBLISHED':
+            print(f'{p["name"]} is not published')
+            continue
+        project = Project(repo=p['repo'], name=p['name'], host=f'https://{p["name"]}.docsqa.jina.ai')
+        if project.host is None:
+            print(f'skip {project}')
+            continue
+        projects_list.append(project)
+    total_future = asyncio.ensure_future(health_check(projects_list))
+
+    result = loop.run_until_complete(total_future)
+    write_to_markdown(result)
+
+
+if __name__ == '__main__':
+    entrypoint()
