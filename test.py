835fdb2f97948ce008ac75c11fd2f1c8e7594117
Nan Wang <nan.wang@jina.ai>
2022-07-19 23:41:11 +0800

    tests: remove the env settings

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-import os
-import pytest
 import json
+import os
 from datetime import datetime
-from app import Project, ProjectJSONEncoder, ProjectJSONDecoder, Status, HealthCheckEvent, entrypoint
+from unittest import mock
+
+import pytest
 from aioresponses import aioresponses
+
+from app import Project, ProjectJSONEncoder, ProjectJSONDecoder, Status, HealthCheckEvent, entrypoint
 
 
 def test_projects_encode_decode(tmp_path):
@@ -26,16 +29,22 @@ def test_projects_encode_decode(tmp_path):
     assert projects == projects_decoded
 
 
-def test_entrypoint(tmp_path):
-    old_data_path = os.environ.get('DATA_PATH', 'data.json')
-    data_path = os.environ['DATA_PATH'] = str(tmp_path / 'data.json')
+@pytest.fixture()
+def tmp_env(tmp_path):
+    with mock.patch.dict(os.environ, {'DATA_PATH': str(tmp_path / 'data.json')}):
+        yield
+
+
+def test_entrypoint(tmp_env):
+    data_path = os.environ['DATA_PATH']
 
     def call_entrypoint():
         with aioresponses() as m:
             projects = [{'repo': f'repo{i}', 'name': f'name{i}', 'status': 'PUBLISHED'} for i in range(5)]
             m.get('https://apidocsqa.jina.ai/projects', status=200, payload=projects)
             for p in projects:
-                m.post(f'https://{p["name"]}.docsqa.jina.ai/search', status=200, payload={'data': [{'matches': 'answer'}]})
+                m.post(f'https://{p["name"]}.docsqa.jina.ai/search', status=200,
+                       payload={'data': [{'matches': 'answer'}]})
             entrypoint()
 
     call_entrypoint()
@@ -49,5 +58,4 @@ def test_entrypoint(tmp_path):
     with open(data_path, 'r') as f:
         data = json.load(f)
         assert all(len(proj['history']) == 2 for proj in data)
-
-    os.environ['DATA_PATH'] = old_data_path
+    print(os.environ['DATA_PATH'])
