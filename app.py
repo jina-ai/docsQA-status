9125121dd5cda029950b3adbed8e490713e3008d
Nan Wang <nan.wang@jina.ai>
2022-07-21 15:17:45 +0800

    fix: minor fix

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def calculate_uptime(project):
                 up_time += hc_event.ctime - cur_time
             cur_time = hc_event.ctime
     total_time = cur_time - project.ctime
-    return up_time.seconds * 100 / total_time.seconds
+    return up_time.seconds * 100 / total_time.seconds if total_time.seconds > 0 else 0
 
 
 def write_to_markdown(projects):
